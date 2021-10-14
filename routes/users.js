6c114f6ca23d76475f20903aec5ffa33325282c2
Marcel Gatete <gatetecodes@gmail.com>
2021-10-14 12:50:13 +0200

    User model created and finished user registration implementation

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,51 @@
 const express = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
 //@route    POST /api/users
 //@desc     registers a user
 //@access   Public
 
-router.post('/', (req, res) => res.json({ msg: 'Register a user' }));
+router.post(
+  '/',
+  [
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email address').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists.' });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User saved!');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: 'Server error' });
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const userRoutes = require('./routes/users');
 const contactsRoutes = require('./routes/contacts');
 const authRoutes = require('./routes/auth');
+const connectDB = require('./config/db');
 
 const app = express();
+
+app.use(express.json({ extended: false }));
+
+//connect database
+connectDB();
 
 //Define routes
 app.use('/api/users', userRoutes);
